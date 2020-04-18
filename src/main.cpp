383d1a56c5bbfdc29177a422f3c8505cc178529e
Andreas Schulz <andi.schulz@me.com>
2020-04-18 21:56:29 +0200

    Send argv[2] from client to server, both blocking

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,7 @@
-#include <array>
 #include <cstdlib>
 #include <iostream>
 
 #include <zmq.h>
-
-constexpr std::size_t BUF_SIZE = 10;
-constexpr std::array<const char, 6> HELLO = {'H', 'E', 'L', 'L', 'O', '\0'};
-constexpr std::array<const char, 6> WORLD = {'W', 'O', 'R', 'L', 'D', '\0'};
-constexpr std::size_t SEND_COUNT = 10;
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
@@ -26,28 +20,49 @@ int main(int argc, char *argv[]) {
             return EXIT_FAILURE;
         }
 
-        while (true) {
-            std::array<char, BUF_SIZE> buffer{};
-            zmq_recv(responder, buffer.data(), buffer.size(), 0);
-            std::cout << "Received Hello\n";
-            zmq_send(responder, WORLD.data(), std::strlen(WORLD.data()), 0);
-        }
+        zmq_msg_t message;
+        zmq_msg_init(&message);
+
+        std::cout << "Waiting for client...\n";
+        zmq_msg_recv(&message, responder, 0);
+
+        std::string data(static_cast<char *>(zmq_msg_data(&message)),
+                         zmq_msg_size(&message));
+        zmq_msg_close(&message);
+
+        std::cout << "Received \"" << data << "\" from client\n";
+
+        zmq_close(responder);
+        zmq_ctx_destroy(context);
     }
 
     else if (arg1 == "-c") {
-        std::cout << "Connecting to hello world server...\n";
+        if (argc < 3) {
+            std::cerr << "Need a message to send\n";
+            return EXIT_FAILURE;
+        }
+
+        std::string data(*std::next(std::next(argv)));
+
+        if (data.empty()) {
+            std::cerr << "Message cannot be empty\n";
+            return EXIT_FAILURE;
+        }
+
+        std::cout << "Sending \"" << data << "\" to hello world server...\n";
+
         void *context = zmq_ctx_new();
         void *requester = zmq_socket(context, ZMQ_REQ);
         zmq_connect(requester, "tcp://localhost:5555");
 
-        std::size_t request_nbr = 0;
-        for (request_nbr = 0; request_nbr != SEND_COUNT; ++request_nbr) {
-            std::array<char, BUF_SIZE> buffer{};
-            std::cout << "Sending Hello " << request_nbr << "...\n";
-            zmq_send(requester, HELLO.data(), std::strlen(HELLO.data()), 0);
-            zmq_recv(requester, buffer.data(), buffer.size(), 0);
-            std::cout << "Received World " << request_nbr << "\n";
-        }
+        zmq_msg_t message;
+        zmq_msg_init_data(&message,
+                          static_cast<void *>(data.data()),
+                          data.size(),
+                          nullptr,
+                          nullptr);
+        zmq_msg_send(&message, requester, 0);
+
         zmq_close(requester);
         zmq_ctx_destroy(context);
     }
